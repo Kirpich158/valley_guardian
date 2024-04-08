02ef7d7310081aa37b6d1be52c450c2125779b44
Kirill_Zub <kirillka.zub@gmail.com>
2024-04-08 18:55:11 +0200

    finished fishing game progress bar logic

--- Assets/src/ui/fishing/FishingGameScript.cs
+++ Assets/src/ui/fishing/FishingGameScript.cs
@@ -3,26 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FishingGameScript : MonoBehaviour {
-    public GameObject zonePrefab;
-    public Transform pointerTransform;
-
-    [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private float _pointerSpeed;
+    [Header("References")]
+    [SerializeField] private GameObject _zonePrefab;
+    [SerializeField] private RectTransform _pointerTrans;
+    [SerializeField] private RectTransform _progressBarTrans;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [Header("Values in %s")]
+    [SerializeField] private float _progressDecay;
     [SerializeField] private float _progressReward;
     [SerializeField] private float _progressPenalty;
 
-    private float _progress = 0;
-    private int _pointerDir = 1;
-    private GameObject _zoneRef;
+    private float _progress;
+    private int _pointerDir;
+    private GameObject _zoneObj;
 
     // parameters to control fishing minigame
     // [SerializeField] private Vector2 zoneSpanwLimit;
 
     private void Start() {
-        _zoneRef = SpawnNewZone();
+        _zoneObj = SpawnNewZone();
+
+        _progress = 0;
+        ResetPointer();
     }
 
     private void Update() {
+        UpdateProgressBar();
         MovePointer();
         CheckPointerHit();
 
@@ -32,60 +39,126 @@ public class FishingGameScript : MonoBehaviour {
         }
     }
 
-    public void ShowGame() {
-        _canvasGroup.alpha = 1; // ==> TODO change for animation clip <==
-        _canvasGroup.interactable = true;
-        _canvasGroup.blocksRaycasts = true;
-    }
-    public void HideGame() {
-        _canvasGroup.interactable = false;
-        _canvasGroup.blocksRaycasts = false;
-        _canvasGroup.alpha = 0; // ==> TODO change for animation clip <==
-    }
-
     private void MovePointer() {
-        if (pointerTransform.localPosition.y >= 245) _pointerDir = -1;
-        if (pointerTransform.localPosition.y <= -245) _pointerDir = 1;
+        if (_pointerTrans.localPosition.y >= 245) _pointerDir = -1;
+        if (_pointerTrans.localPosition.y <= -245) _pointerDir = 1;
 
-        pointerTransform.localPosition = new Vector2(pointerTransform.localPosition.x, pointerTransform.localPosition.y + _pointerSpeed * Time.deltaTime * _pointerDir);
+        _pointerTrans.localPosition = new Vector2(_pointerTrans.localPosition.x, _pointerTrans.localPosition.y + _pointerSpeed * Time.deltaTime * _pointerDir);
+    }
+
+    private void ResetPointer() {
+        _pointerDir = 0;
+        _pointerTrans.localPosition = new Vector2(_pointerTrans.localPosition.x, 0);
     }
 
     private GameObject SpawnNewZone() {
         int nextY = Random.Range(-200, 200);
 
-        GameObject instance = Instantiate(zonePrefab, gameObject.transform);
+        GameObject instance = Instantiate(_zonePrefab, gameObject.transform);
         instance.transform.localPosition = new Vector2(0, nextY);
-        instance.transform.SetSiblingIndex(pointerTransform.GetSiblingIndex());
+        instance.transform.SetSiblingIndex(_pointerTrans.GetSiblingIndex());
 
         return instance;
     }
 
     private void CheckPointerHit() {
         if (UIManagerScript.Instance.Fishing && Input.GetKeyDown(KeyCode.Space)) {
-            if (_zoneRef == null) return;
-            float zoneBot = _zoneRef.transform.localPosition.y - _zoneRef.GetComponent<RectTransform>().rect.height / 2;
-            float zoneTop = _zoneRef.transform.localPosition.y + _zoneRef.GetComponent<RectTransform>().rect.height / 2;
-            if (pointerTransform.localPosition.y < zoneTop && pointerTransform.localPosition.y > zoneBot) {
-                // progress reward
+            if (_zoneObj == null) return;
+            float zoneBot = _zoneObj.transform.localPosition.y - _zoneObj.GetComponent<RectTransform>().rect.height / 2;
+            float zoneTop = _zoneObj.transform.localPosition.y + _zoneObj.GetComponent<RectTransform>().rect.height / 2;
+            if (_pointerTrans.localPosition.y < zoneTop && _pointerTrans.localPosition.y > zoneBot) {
+                AddToProgress(_progressReward); // progress reward
+                //IncreaseTheProgress();
 
                 // deletion of old hit zone
-                if (_zoneRef != null) {
-                    Destroy(_zoneRef);
-                    _zoneRef = null;
-                    _zoneRef = SpawnNewZone();
+                if (_zoneObj != null) {
+                    Destroy(_zoneObj);
+                    _zoneObj = null;
+                    _zoneObj = SpawnNewZone();
                 }
-            }
-            else {
-                // progress penalty
+            } else {
+                AddToProgress(-_progressPenalty); // progress penalty
+                // DecreaseTheProgress(); 
             }
         }
     }
 
-    private void IncreaseTheProgress() {
+    private void UpdateProgressBar() {
+        if (_progress >= 100) {
+            _progress = 100;
+            _progressBarTrans.sizeDelta = new Vector2(_progressBarTrans.sizeDelta.x, Mathf.Lerp(40, 490, _progress / 100));
+            UIManagerScript.Instance.HideFishingGame();
+        }
 
+        if (_progress > 0 && _progress < 100 && _pointerDir != 0) AddToProgress(-Time.deltaTime * _progressDecay);
+        else if (_progress <= 0) _progress = 0;
+
+        _progressBarTrans.sizeDelta = new Vector2(_progressBarTrans.sizeDelta.x, Mathf.Lerp(40, 490, _progress / 100));
     }
 
-    private void DecreaseTheProgress() {
+    private void AddToProgress(float value) {
+        _progress += value;
+    }
 
+    #region Old handmade linear interpolation try // COMEBACK TO IT
+    //private void UpdateProgressBar() {
+    //    if (_progressBarTrans.rect.height >= 490) {
+    //        Debug.Log("fishing succeed");
+    //        _progressBarTrans.sizeDelta = new Vector2(_progressBarTrans.sizeDelta.x, 490);
+    //        UIManagerScript.Instance.HideFishingGame();
+    //    }
+
+    //    if (_progressBarTrans.rect.height > 40 && _progressBarTrans.rect.height < 490 && _pointerDir != 0) {
+    //        AddToProgressVal(-Time.deltaTime * _progressDecaySpd);
+    //    } else {
+    //        _progressBarTrans.sizeDelta = new Vector2(
+    //            _progressBarTrans.sizeDelta.x,
+    //            40
+    //        );
+    //    }
+    //}
+
+    //private void IncreaseTheProgress() {
+    //    if (490 - _progressBarTrans.rect.height >= _progressReward) {
+    //        AddToProgressVal(_progressReward);
+    //        return;
+    //    }
+    //    AddToProgressVal(490 - _progressBarTrans.rect.height);
+    //}
+
+    //private void DecreaseTheProgress() {
+    //    if (_progressBarTrans.rect.height - 40 >= _progressPenalty) {
+    //        AddToProgressVal(-_progressPenalty);
+    //        return;
+    //    }
+    //    AddToProgressVal(-(_progressBarTrans.rect.height - 40));
+    //}
+
+    //private void AddToProgressVal(float value) {
+    //    _progressBarTrans.sizeDelta = new Vector2(
+    //        _progressBarTrans.sizeDelta.x,
+    //        _progressBarTrans.sizeDelta.y + value
+    //    );
+    //}
+    #endregion
+
+    public void ShowGame() {
+        // _progress = PlayerMain.Instance.Equipment.(ItemType.FishingRod)Equipments[6].fishingBoost;
+        
+        _canvasGroup.alpha = 1; // ==> TODO change for animation clip <==
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+        
+        // start pointer after animation clip [^^^] ended
+        _pointerDir = 1;
+    }
+    public void HideGame() {
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
+        _canvasGroup.alpha = 0; // ==> TODO change for animation clip <==
+
+        // reset these after animation clip [^^^] ended
+        _progress = 0;
+        ResetPointer();
     }
 }
